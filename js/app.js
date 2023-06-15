f7244129f1204d0791ced967875edf586654171c
EmilioRubiolo <emilio.rubiolo.git@gmail.com>
2023-06-15 16:59:54 -0300

    Entrega 1

--- js/app.js
+++ js/app.js
@@ -1,73 +1,14 @@
-///mostrar por consola los numeros del 1 al 10
-
-/* let i = 1;
-
-console.log(i);
-i = i+1;
-console.log(i);
-i = i+1;
-console.log(i);
-i = i+1;
-console.log(i);
-i = i+1; */
-
-
-///el codigo lo voy a tener que repetir 10 veces
-
-////FOR
-///necesitamos saber la cantidad de veces que vamos a iterar
-/* 
- for(VALOR INICIAL; CONDICION_CONTINUIDAD; AVANCE)
- {
-    porcion codigo
- }
-
-*/
-
-///mostrar por consola los numeros del 1 al 10
-///podemos declarar la variable en su estructura, pero una vez que termine el for, 
-///la variable no existe mas
-
-
-///operador incremento
-/// i = i + 1  o   i++
-
-/* for (let i = 1; i <= 10; i++) {
-    console.log(i);
-}
- */
-/* 
-1 - inicializa i en 1
-2 - evalua la condicion central
-3- ejecutar las sentencias de codigo
-4 - incrementa i en 1
-5 - evalua condicion central
-6 - ejecutar las sentencias de codigo
-*/
-
-
-
-////BUCLE WHILE 
-/* 
- while (CONDICION_VERDADERA) {
-    EJECUTAN SENTENCIAS
- }
-
-*/
-
-
-let continuar; 
-let precio;
-let cantidad;
 let total = 0;
+function productoTiend(costo, cantidad, totalProducto, producto) {
+    return totalProducto + costo*cantidad + producto;
+}
+iniciar = "si";
 
 do {
-    console.log('agregando un nuevo producto al carrito de compras...');
-    precio = parseFloat(prompt('precio del producto: '));
-    cantidad = parseInt(prompt('cantidad: '));
-    total = total + precio*cantidad;
-    continuar = prompt('Queres agregar otro producto en el carrito de compras? si/basta');
-}while(continuar == 'si');
-
-
-alert('El total del carrito es $'+ total); 
+    nombreProducto = prompt("Nombre del producto");
+    precioProducto = parseFloat(prompt("Costo del producto"));
+    cantidadProducto = parseInt(prompt("Ingresar cantidad"));
+    total = productoTienda(precioProducto, cantidadProducto, total, precioProducto);
+    console.log(total);
+    iniciar = prompt("Desea continuar? si/no")
+} while (iniciar !== "no")
