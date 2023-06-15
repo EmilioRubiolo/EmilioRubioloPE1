35ce0867f48f86007b61a6e86ba10af5248a0a7c
EmilioRubiolo <emilio.rubiolo.git@gmail.com>
2023-06-15 18:15:11 -0300

    Pre entrega, carrito

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
+let precioItem;
+let nombreItem;
+let cantidad;
+let nombreUser;
 let total = 0;
-function productoTiend(costo, cantidad, totalProducto, producto) {
-    return totalProducto + costo*cantidad + producto;
-}
-iniciar = "si";
+let opcion;
+let ticket = "" ;
 
-do {
-    nombreProducto = prompt("Nombre del producto");
-    precioProducto = parseFloat(prompt("Costo del producto"));
-    cantidadProducto = parseInt(prompt("Ingresar cantidad"));
-    total = productoTienda(precioProducto, cantidadProducto, total, precioProducto);
-    console.log(total);
-    iniciar = prompt("Desea continuar? si/no")
-} while (iniciar !== "no")
+alert('Bienvenido al carrito de compras');
+nombreUser = prompt("Ingrese su nombre");
+
+do{
+    nombreItem = prompt("Nombre del item");
+    precioItem = Number(prompt("Ingresar el precio del item"));
+
+    while (precioItem < 0 ) {
+        precioItem = Number(prompt('Precio invalido'));
+    }
+
+    cantidad = parseInt(prompt("Ingrese la cantidad"));
+
+    while (cantidad <= 0 ) {
+        cantidad = parseInt(prompt('Cantidad invalida'));
+    }
+    ticket = ticket + `nombre del item : ${nombreItem} \n precio unitario: $${precioItem} \n cantidad: ${cantidad} \n subtotal: $${cantidad * precioItem} \n\n`
+    total = total + cantidad * precioItem;
+
+    opcion = prompt("¿Desea añadir otro item?");
+
+} while (opcion == 'si')
+
+alert(`${ticket} El total para ${nombreUser} es de: $${total}`);
